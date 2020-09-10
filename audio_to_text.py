aaffc6230a9c42c73fe69951dc6d6a81384fdcb7
robriu <ro.briuv@gmail.com>
2020-09-10 21:11:47 +0200

    dev/initial commit

---
+++ audio_to_text.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+print('[INFO] Program initialized.')
+
+with sr.AudioFile("data_input.wav") as data_input:
+		print ('[UPDATE] Preparing a sound file...')
+		data_output = recognizer.record(data_input)
+
+try:
+		print ('[UPDATE] Extracting text...')
+		script = recognizer.recognize_google(data_output)
+
+		print ('[UPDATE] Creating an output file...')
+		output_file = open("audio_output.txt", "w", encoding='utf-8')
+
+		output_file.writelines(script + "\n")
+		output_file.close()
+		print ('[UPDATE] Text Extration Done...')
+
+except Exception as e:
+		print("[FAILURE]: " + str(e))
+
+print('[SUCCESS] Execution Done.')
+print('[INFO] Program Closed.')
