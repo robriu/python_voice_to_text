9b3cca37d5690bbac0685b27b6b4e837461cad3e
robriu <ro.briuv@gmail.com>
2020-09-17 23:01:46 +0200

    dev/add git .ignore

--- audio_to_text.py
+++ audio_to_text.py
@@ -4,22 +4,22 @@ recognizer = sr.Recognizer()
 print('[INFO] Program initialized.')
 
 with sr.AudioFile("data_input.wav") as data_input:
-	print ('[UPDATE] Preparing a sound file...')
-	data_output = recognizer.record(data_input)
+    print ('[UPDATE] Preparing a sound file...')
+    data_output = recognizer.record(data_input)
 
 try:
-	print ('[UPDATE] Extracting text...')
-	script = recognizer.recognize_google(data_output)
+    print ('[UPDATE] Extracting text...')
+    script = recognizer.recognize_google(data_output)
 
-	print ('[UPDATE] Creating an output file...')
-	output_file = open("audio_output.txt", "w", encoding='utf-8')
+    print ('[UPDATE] Creating an output file...')
+    output_file = open("audio_output.txt", "w", encoding='utf-8')
 
-	output_file.writelines(script + "\n")
-	output_file.close()
-	print ('[UPDATE] Text Extration Done...')
+    output_file.writelines(script + "\n")
+    output_file.close()
+    print ('[UPDATE] Text Extration Done...')
 
 except Exception as e:
-	print("[FAILURE]: " + str(e))
+    print("[FAILURE]: " + str(e))
 
 print('[SUCCESS] Execution Done.')
 print('[INFO] Program Closed.')
