87dc4e36c233ea746f2dac198b61eb3aad90187f
robriu <ro.briuv@gmail.com>
2020-10-03 12:31:16 +0200

    dev/improve code

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,26 +1,41 @@
 import speech_recognition as sr
 
+AIFF = [".aiff", ".aif"]
+AIFFC = [".aifc"]
+WAV = [".wav", ".wave"]
+FLAC = [".flac"]
+
+FORMATS_SUPPORTED = tuple(AIFF + AIFFC + WAV + FLAC)
+
 recognizer = sr.Recognizer()
+
 print('[INFO] This program exactracts scripts from audio file.')
+input_filename = input("[INPUT] Enter input filename: ")
 
-# example filename "data_input.wav"
-with sr.AudioFile(input("[INPUT] Enter filename: ")) as data_input:
-    print ('[UPDATE] Recognizing an audio file...')
-    data_output = recognizer.record(data_input)
+if not input_filename.lower().endswith(FORMATS_SUPPORTED):
+    print("[FAILURE] Not supported file format.")
+    
+else:
+    try:
+        # example input filename "data_input.wav"
+        with sr.AudioFile(input_filename) as data_input:
+            print ('[UPDATE] Recognizing an audio file...')
+            data_output = recognizer.record(data_input)
+    
+        print ('[UPDATE] Extracting a script...')
+        script = recognizer.recognize_google(data_output)
 
-try:
-    print ('[UPDATE] Extracting a script...')
-    script = recognizer.recognize_google(data_output)
+        print ('[UPDATE] Creating an output file...')
+        # example output filename "audio_output.txt"
+        output_file = open(input("[INPUT] Enter output filename: "), "w", encoding = 'utf-8')
 
-    print ('[UPDATE] Creating an output file...')
-    output_file = open("audio_output.txt", "w", encoding = 'utf-8')
+        output_file.writelines(script + "\n")
+        output_file.close()
 
-    output_file.writelines(script + "\n")
-    output_file.close()
-    print ('[UPDATE] Script Extracted...')
+        print ('[UPDATE] Script Extracted...')
+        print('[SUCCESS] Execution Done.')
 
-except Exception as e:
-    print("[FAILURE]: " + str(e))
-
-print('[SUCCESS] Execution Done.')
+    except Exception as e:
+        print("[FAILURE]: " + str(e).replace("; ", "\r\n\t"))
+    
 print('[INFO] Program Finished.')
